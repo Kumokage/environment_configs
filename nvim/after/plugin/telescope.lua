4180b29a704d90a7b43f5d87eb55152ebcdfe7a2
Alex Glushko <aglushko2@slb.com>
2024-02-01 12:59:34 +0300

    Quick fix of telescope mappings and fugitive

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,13 +1,12 @@
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set("n", "<leader>pc", builtin.grep_string, {}) -- find string in current working directory as you type
 vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})   -- find string under cursor in current working directory
 vim.keymap.set("n", "<leader>po", builtin.lsp_document_symbols, {})
-vim.keymap.set("n", "<leader>pO", builtin.current_buffer_fuzzy_find, {})
+vim.keymap.set("n", "<leader>pc", builtin.current_buffer_fuzzy_find, {})
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 
+vim.keymap.set('n', '<leader>gs', builtin.git_files, {})
 vim.keymap.set("n", "<leader>gc", builtin.git_commits, {})
 vim.keymap.set("n", "<leader>gfc", builtin.git_bcommits, {})
 vim.keymap.set("n", "<leader>gb", builtin.git_branches, {})
