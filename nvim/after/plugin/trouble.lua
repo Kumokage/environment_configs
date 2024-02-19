dbad86ca353c8b04c4336e6d38ff0be67f8a3450
Alex Glushko <aglushko2@slb.com>
2024-02-19 15:16:24 +0300

    Add replace trouble remap

--- nvim/after/plugin/trouble.lua
+++ nvim/after/plugin/trouble.lua
@@ -1,3 +1,2 @@
-vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
-  {silent = true, noremap = true}
-)
+vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
+vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
